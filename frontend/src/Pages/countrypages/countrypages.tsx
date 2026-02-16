506c941b34dac77376b31759f4c65af68ac02e7a
Whlxon <cyril.houppertz@gmaiL.com>
2026-02-16 16:20:47 +0100

    Securise API key

--- frontend/src/Pages/countrypages/countrypages.tsx
+++ frontend/src/Pages/countrypages/countrypages.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom";
 import { saveConversation, readConversationByCountry } from "../utils/conversations";
 
-import { apiKey } from "../../apiKey";
+const apiKey = import.meta.env.MAMMOUTH_APIKEY;
 
 import '../../index.css'
 
