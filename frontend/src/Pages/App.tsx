89c0fa7967e32efe522b0d854f6ffaf8e5d5af8d
Whlxon <cyril.houppertz@gmaiL.com>
2026-02-19 16:34:29 +0100

    Change

--- frontend/src/Pages/App.tsx
+++ frontend/src/Pages/App.tsx
@@ -1,12 +1,10 @@
 import { Outlet,  } from 'react-router-dom';
 import './App.css'
-import { Header } from './header';
 
 function App() {
 
   return (
     <>
-      <Header/>
       <Outlet/>
     </>
   )
